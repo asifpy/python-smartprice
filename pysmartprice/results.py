7c22b124bad7c2245769ef5547640a008bd5d9d0
Asif <saluasif@gmail.com>
2016-01-10 09:25:35 +0300

    method rename

--- pysmartprice/results.py
+++ pysmartprice/results.py
@@ -5,5 +5,6 @@ class SmartPriceResult(object):
     def __init__(self, params):
         self.__dict__.update(params)
 
-    def dumpjson(self):
+    @property
+    def dumptojson(self):
         return json.dumps(self.__dict__)
