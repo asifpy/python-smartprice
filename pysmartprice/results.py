4c87396919adfff56976df0cb65569c959c18f00
Asif <saluasif@gmail.com>
2016-01-10 09:21:14 +0300

    convert result instance to json

--- pysmartprice/results.py
+++ pysmartprice/results.py
@@ -1,3 +1,9 @@
+import json
+
+
 class SmartPriceResult(object):
     def __init__(self, params):
         self.__dict__.update(params)
+
+    def dumpjson(self):
+        return json.dumps(self.__dict__)
