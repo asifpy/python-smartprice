9f02a59b6a02e1cabb064f1c37a6b2a621d87362
Asif <saluasif@gmail.com>
2016-01-27 11:34:07 +0300

    pretty json

--- pysmartprice/results.py
+++ pysmartprice/results.py
@@ -1,13 +1,31 @@
 import json
 
 
+class ComplexEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if hasattr(obj, 'jsonable'):
+            return obj.dumptojson
+        else:
+            return json.JSONEncoder.default(self, obj)
+
+
 class SmartPriceBase(object):
     def __init__(self, params):
         self.__dict__.update(params)
 
+    def jsonable(self):
+        return self.__dict__
+
     @property
     def dumptojson(self):
-        return json.dumps(self.__dict__)
+        return json.loads(
+            json.dumps(
+                self.jsonable(),
+                cls=ComplexEncoder,
+                # sort_keys=True,
+                indent=4,
+                separators=(',', ': '))
+            )
 
 
 class SmartPriceResult(SmartPriceBase):
